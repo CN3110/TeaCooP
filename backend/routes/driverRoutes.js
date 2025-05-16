795869fff320e03ee31497a09aa62c9a3ba9e1eb
CN3110 <nimesha-im21096@stu.kln.ac.lk>
2025-05-17 02:02:46 +0530

    feat: Update driver fetching endpoint to retrieve only active drivers for delivery record dropdown

--- backend/routes/driverRoutes.js
+++ backend/routes/driverRoutes.js
@@ -6,9 +6,11 @@ const router = express.Router();
 //crud
 router.get("/", getAllDrivers);
 router.post("/add", addDriver);
+
+router.get("/active", getActiveDrivers); 
 router.put("/:driverId", updateDriver);
 router.get("/:driverId", getDriverById);
-router.get("/active/list", getActiveDrivers); 
+
 router.put("/:driverId/disable", disableDriver);  // Disable driver
 
 router.put("/:driverId/password", updatePassword);
